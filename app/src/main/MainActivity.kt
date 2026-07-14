b946777d1288df5e588f86b28122f3a8aa84636e
Marshal549 <privatnik@protonmail.com>
2026-07-14 12:22:15 +0300

    Add MainActivity
    Initial implementation of MainActivity for video player.

---
+++ app/src/main/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kinovolt
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Тут буде логіка нашого відео-плеєра
+    }
+}
